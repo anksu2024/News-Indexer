827918735f67cbdae296dbe57fdf5c05f97895aa
Ankit <ankitsarraf@Ankits-MacBook-Pro.local>
2015-01-21 23:00:20 -0500

    Comments in Runner

--- newsindexer-master/edu/buffalo/cse/irf14/Runner.java
+++ newsindexer-master/edu/buffalo/cse/irf14/Runner.java
@@ -1,3 +1,7 @@
+/**
+ * AUTHOR: Ankit Sarraf
+ * DATED : January 21, 2014
+ */
 package edu.buffalo.cse.irf14;
 
 import java.io.File;
@@ -8,39 +12,51 @@ import edu.buffalo.cse.irf14.document.ParserException;
 import edu.buffalo.cse.irf14.index.IndexWriter;
 import edu.buffalo.cse.irf14.index.IndexerException;
 
-/**
- * @author nikhillo
- */
-
 public class Runner {
 	public Runner() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		String ipDir = args[0];
+		// The argument 1 indicates the Path of news dump
+		String inputDir = args[0];
+
+		// The argument 2 indicates the Path of the indexed files
 		String indexDir = args[1];
-		//more? idk!
 
-		File ipDirectory = new File(ipDir);
-		String[] catDirectories = ipDirectory.list();
+		//more? idk! => PART OF STARTER CODE. DON'T KNOW WHAT IT REPRESENTS.
 
-		String[] files;
+		// Reach the path on the news dump
+		File inputDirectory = new File(inputDir);
+
+		// Get list of all the directories (Categories) in input new dump path
+		String [] catDirectories = inputDirectory.list();
+
+		// This would store the path of each list of files in categories
+		String [] files;
+
+		// Will fetch each category directory
 		File dir;
 
+		// Some initial Declarations to be understood later
 		Document d = null;
 		IndexWriter writer = new IndexWriter(indexDir);
 
 		try {
+			// Iterate over each categories Directories)
 			for (String cat : catDirectories) {
-				dir = new File(ipDir+ File.separator+ cat);
+				// File.separator provides value dependant of the OS
+				// For UNIX/ MAC it is '/'
+				// For Windows it is '\\'
+				dir = new File(inputDir + File.separator + cat);
+
+				// List of files in current catergory directory
 				files = dir.list();
 
-				if (files == null)
+				if (files == null) {
+					// If the directory has no files move on to next category
 					continue;
+				}
 
 				for (String f : files) {
 					try {
@@ -52,6 +68,7 @@ public class Runner {
 					} 
 				}
 			}
+
 			writer.close();
 		} catch (IndexerException e) {
 			// TODO Auto-generated catch block
