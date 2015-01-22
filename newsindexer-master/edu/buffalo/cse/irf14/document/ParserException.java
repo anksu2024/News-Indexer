7e66c43b049dee596759a57b7a135bddfe718cb1
Ankit <ankitsarraf@Ankits-MacBook-Pro.local>
2015-01-21 23:09:30 -0500

    Added the getMessage Method

--- newsindexer-master/edu/buffalo/cse/irf14/document/ParserException.java
+++ newsindexer-master/edu/buffalo/cse/irf14/document/ParserException.java
@@ -1,17 +1,15 @@
 /**
- * 
+ * AUTHOR : ANKIT SARRAF
+ * DATED  : January 21, 2014
+ * ABOUT  : Generic wrapper exception class for parsing exceptions
  */
+
 package edu.buffalo.cse.irf14.document;
 
-/**
- * @author nikhillo
- * Generic wrapper exception class for parsing exceptions
- */
 public class ParserException extends Exception {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4691717901217832517L;
-
+	
+	public String getMessage() {
+		return "Some Parser Exception";
+	}
 }
