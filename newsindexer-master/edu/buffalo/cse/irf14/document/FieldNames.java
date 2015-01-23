ab8763ffe8e7766eb28947d2a0f55710ab4388e9
Ankit <ankitsarraf@Ankits-MacBook-Pro.local>
2015-01-22 21:47:16 -0500

    File Formatted

--- newsindexer-master/edu/buffalo/cse/irf14/document/FieldNames.java
+++ newsindexer-master/edu/buffalo/cse/irf14/document/FieldNames.java
@@ -1,12 +1,17 @@
 /**
- * 
+ * AUTHOR : Ankit Sarraf
+ * DATED  : January 22, 2015
+ * ABOUT  : This is an enumeration that defines the different field names
  */
 package edu.buffalo.cse.irf14.document;
 
-/**
- * @author nikhillo
- * This is an enumeration that defines the different field names
- */
 public enum FieldNames {
-	FILEID, CATEGORY, TITLE, AUTHOR, AUTHORORG, PLACE, NEWSDATE, CONTENT
+	FILEID,			// Unique identifier of document (Filename)
+	CATEGORY,		// Category to which document belongs (Sub Directory Name)
+	TITLE,			// Title of Document (Usually the first line Capitalized)
+	AUTHOR,			// Author of Document (Optional present in <AUTHOR> tag)
+	AUTHORORG,		// Author's organization (Also contained in <AUTHOR> tag)
+	PLACE,			// Present as first word in news text (Just after <AUTHOR>)
+	NEWSDATE,		// Date on which news was reported (Formatted as MMMMM dd)
+	CONTENT			// Actual News content (Rest of article without metadata)
 };
