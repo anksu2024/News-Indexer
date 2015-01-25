193dd7db26a37df33ed9e3bf5fad29fa8ae929b5
Ankit <ankitsarraf@Ankits-MacBook-Pro.local>
2015-01-25 13:31:54 -0500

    Proper commenting of the TokenFilterType

--- newsindexer-master/edu/buffalo/cse/irf14/analysis/TokenFilterType.java
+++ newsindexer-master/edu/buffalo/cse/irf14/analysis/TokenFilterType.java
@@ -1,12 +1,18 @@
 /**
- * 
+ * AUTHOR : Ankit Sarraf
+ * DATED  : January 25, 2015
+ * ABOUT  : Enum that defines different {@link TokenFilter} types
  */
+
 package edu.buffalo.cse.irf14.analysis;
 
-/**
- * @author nikhillo
- * Enum that defines different {@link TokenFilter} types
- */
 public enum TokenFilterType {
-	SYMBOL, DATE, NUMERIC, CAPITALIZATION, STOPWORD, STEMMER, ACCENT, SPECIALCHARS
+	SYMBOL,			// Punctuations, Apostrophes, Hyphens
+	DATE,			// yyyymmdd (Dates) HH:mm:ss (Time)
+	NUMERIC,		// Any number that is not a date
+	CAPITALIZATION,	// Rules for Camel Cased Words and Abbreviations
+	STOPWORD,		// Standard Stop word list removed
+	STEMMER,		// Replace the word with the stemmer version
+	ACCENT,			// Accents, Diacritics => Corresponding English Characters
+	SPECIALCHARS	// Anything that is not a number or an alphabet
 };
