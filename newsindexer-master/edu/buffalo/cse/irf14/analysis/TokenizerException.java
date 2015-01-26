b3b0fb1b3902a93b5df125d23a5a20edad14ddde
Ankit <ankitsarraf@Ankits-MacBook-Pro.local>
2015-01-26 16:38:57 -0500

    Asthetic Beauty of Code

--- newsindexer-master/edu/buffalo/cse/irf14/analysis/TokenizerException.java
+++ newsindexer-master/edu/buffalo/cse/irf14/analysis/TokenizerException.java
@@ -1,17 +1,12 @@
 /**
- * 
+ * AUTHOR : Ankit Sarraf
+ * DATED  : January 26, 2015
+ * ABOUT  : Wrapper exception class for any errors during Tokenization
  */
+
 package edu.buffalo.cse.irf14.analysis;
 
-/**
- * @author nikhillo
- * Wrapper exception class for any errors during Tokenization
- */
 public class TokenizerException extends Exception {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 215747832619773661L;
 
 }
