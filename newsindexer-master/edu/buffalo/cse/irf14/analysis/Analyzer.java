77d990d6bbfc45ce6328d8bce7a86f390e3363e9
Ankit <ankitsarraf@Ankits-MacBook-Pro.local>
2015-01-29 00:52:14 -0500

    Analyzer Complete

--- newsindexer-master/edu/buffalo/cse/irf14/analysis/Analyzer.java
+++ newsindexer-master/edu/buffalo/cse/irf14/analysis/Analyzer.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.buffalo.cse.irf14.analysis;
 
 import edu.buffalo.cse.irf14.document.FieldNames;
@@ -12,14 +9,17 @@ import edu.buffalo.cse.irf14.document.FieldNames;
  * 	- Either be implemented as a single {@link TokenFilter} instance
  *  - Or implemented as a TokenFilter chain that handles {@link FieldNames} specific processing
  */
+
 public interface Analyzer {
 	/**
 	 * Method to indicate that the implementing class must complete 
-	 * all its processing on the current {@link Token} and proceed to next 
+	 * all its processing on the current {@link Token} and proceed to next
+	 * @return true if a Token exists in the stream that is unprocessed, 
+	 * false if no more unprocessed tokens exist 
 	 * @throws TokenizerException : If any exception occurs during the operation
 	 */
-	public abstract void increment() throws TokenizerException;
-	
+	public abstract boolean increment() throws TokenizerException;
+
 	/**
 	 * Return the underlying {@link TokenStream} instance
 	 * @return The underlying stream
