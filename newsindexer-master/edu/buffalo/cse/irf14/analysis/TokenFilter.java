5a187cfea09e5c2b173906d764b397ff656a38bb
Ankit Sarraf <sarrafankit@hotmail.com>
2015-02-23 23:25:13 -0500

    TokenFilter completed

--- newsindexer-master/edu/buffalo/cse/irf14/analysis/TokenFilter.java
+++ newsindexer-master/edu/buffalo/cse/irf14/analysis/TokenFilter.java
@@ -1,23 +1,24 @@
 /**
- * 
+ * AUTHOR : Ankit Sarraf
+ * DATED  : January 25, 2015
+ * ABOUT  : The abstract class that you must extend when implementing your 
+ *          TokenFilter rule implementations.
+ *          Apart from the inherited Analyzer methods, we would use the 
+ *          inherited constructor (as defined here) to test your code.
  */
+
 package edu.buffalo.cse.irf14.analysis;
 
-/**
- * The abstract class that you must extend when implementing your 
- * TokenFilter rule implementations.
- * Apart from the inherited Analyzer methods, we would use the 
- * inherited constructor (as defined here) to test your code.
- * @author nikhillo
- *
- */
 public abstract class TokenFilter implements Analyzer {
+	protected TokenStream tokenStream;
+
 	/**
 	 * Default constructor, creates an instance over the given
 	 * TokenStream
 	 * @param stream : The given TokenStream instance
 	 */
-	public TokenFilter(TokenStream stream) {
-		//TODO : YOU MUST IMPLEMENT THIS METHOD
+
+	public TokenFilter(TokenStream tokenStream) {
+		this.tokenStream = tokenStream;
 	}
 }
